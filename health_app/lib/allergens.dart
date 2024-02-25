b43b720ba6a8c9a572df85130ad49a3822c752aa
Rishi Vora <87199706+rishijv@users.noreply.github.com>
2024-02-24 17:02:34 -0800

    Update allergens.dart

--- health_app/lib/allergens.dart
+++ health_app/lib/allergens.dart
@@ -1,1 +1,129 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:openfoodfacts/openfoodfacts.dart';
+import 'package:simple_barcode_scanner/simple_barcode_scanner.dart';
 
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBody: true,
+      appBar: AppBar(),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              name,
+              style: Theme.of(context).textTheme.headlineLarge,
+            ),
+            Text(
+              ingredients,
+              style: const TextStyle(fontSize: 10),
+            ),
+            Text(
+              canEat,
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            Text(
+              whichAllergens,
+              style: Theme.of(context).textTheme.headlineSmall,
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                // ignore: prefer_typing_uninitialized_variables
+                var tempString;
+                var res = await Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const SimpleBarcodeScannerPage(),
+                    ));
+                if (res is String) {
+                  tempString = res;
+                }
+
+                var product = await OpenFoodAPIClient.getProductV3(
+                  ProductQueryConfiguration(tempString,
+                      version: ProductQueryVersion.v3),
+                );
+
+                setState(() {
+                  if (product.product != null) {
+                    name =
+                        "Product: ${product.product?.getBestProductName(OpenFoodFactsLanguage.ENGLISH) /*getProductNameBrand(OpenFoodFactsLanguage.ENGLISH, " ")*/}";
+                    ingredients =
+                        product.product!. /*productName!*/ ingredientsText!;
+                    List<Ingredient>? ingredientsList =
+                        product.product?.ingredients;
+                    List<String>? allergensList = [];
+                    whichAllergens = "";
+
+                    for (int i = 0; i < allergies.length; i++) {
+                      allergensList.add("");
+                    }
+
+                    for (int i = 0; i < ingredientsList!.length; i++) {
+                      String? currentIngredient = ingredientsList[i].text;
+
+                      for (int j = 0; j < allergies.length; j++) {
+                        //whichAllergens += "${currentIngredient!.toUpperCase()}-${allergies[j].toUpperCase()}/";
+                        if (allergensList[j].compareTo("") == 0 ||
+                            allergensList[j].contains("No")) {
+                          if (currentIngredient!
+                              .toUpperCase()
+                              .contains(allergies[j].toUpperCase())) {
+                            allergensList.insert(
+                                j, "Has Allergen: $currentIngredient\n");
+
+                            allergensList.removeAt(j + 1);
+                          } else {
+                            allergensList.insert(
+                                j, "No Allergen: ${allergies[j]}\n");
+
+                            allergensList.removeAt(j + 1);
+                          }
+                        }
+                      }
+                    }
+
+                    for (int i = 0; i < allergensList.length; i++) {
+                      if (!allergensList[i].contains("No")) {
+                        whichAllergens += allergensList[i];
+                      }
+                    }
+
+                    if (whichAllergens.compareTo("") == 0) {
+                      canEat = "You can eat this";
+                    } else {
+                      canEat = "You can't eat this";
+                    }
+                  } else {
+                    name = "Error With Scanning";
+                    whichAllergens = "";
+                    ingredients = "Please Try Again";
+                  }
+
+                  // for(int i = 0; i < allergies.length; i++){
+                  //   if(ingredients.toUpperCase().contains(allergies[i].toUpperCase())){
+                  //     whichAllergens += "Has Allergen: ${allergies[i]}\n";
+                  //   }else{
+                  //     whichAllergens += "No Allergen: ${allergies[i]}\n";
+                  //   }
+                  // }
+                });
+              },
+              child: const Text('Open Scanner'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
