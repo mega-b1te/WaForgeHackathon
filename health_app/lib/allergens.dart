919445c6db4181f1765f6aa54dd401972ffafde5
Rishi Vora <87199706+rishijv@users.noreply.github.com>
2024-02-25 12:21:11 -0800

    allergens page setup

--- health_app/lib/allergens.dart
+++ health_app/lib/allergens.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:health_app/theme.dart';
 import 'package:openfoodfacts/openfoodfacts.dart';
 import 'package:simple_barcode_scanner/simple_barcode_scanner.dart';
+import 'package:health_app/home.dart';
 
 class Allergens extends StatefulWidget {
   final String name;
@@ -29,38 +31,92 @@ class AllergensState extends State<Allergens> {
   Widget build(BuildContext context) {
     return Scaffold(
       extendBody: true,
-      appBar: AppBar(),
+      //appBar: AppBar(),
       body: Container(
-        padding: const EdgeInsets.all(10),
-        decoration: BoxDecoration(color: ThemeClass().primaryColor),
-        child: Container(
-          color: ThemeClass().wColor,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                widget.name,
-                style: Theme.of(context).textTheme.headlineLarge,
+        color: ThemeClass().primaryColor,
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(20, 60, 20, 20),
+          child: Container(
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+                color: ThemeClass().wColor,
+                borderRadius: BorderRadius.circular(40.0)),
+            child: Container(
+              //color: ThemeClass().bColor,
+              padding: const EdgeInsets.all(10),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        const SizedBox(
+                          width: 40.0,
+                        ),
+                        const Spacer(),
+                        Row(
+                          children: <Widget>[
+                            ListView(
+                              children: [
+                                SizedBox(
+                                  height: 80,
+                                  child: Text(
+                                    widget.name,
+                                    style: Theme.of(context)
+                                        .textTheme
+                                        .headlineLarge,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                        const Spacer(),
+                        FloatingActionButton.small(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => const MyHomePage(),
+                              ),
+                            );
+                          },
+                          backgroundColor: ThemeClass().primaryColor,
+                          foregroundColor: ThemeClass().bColor,
+                          child: const Icon(
+                            Icons.close,
+                          ),
+                        ),
+                      ]),
+                  Expanded(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        //const Spacer(),
+                        Text(
+                          widget.ingredients,
+                          style: const TextStyle(fontSize: 10),
+                        ),
+                        Text(
+                          widget.canEat,
+                          style: Theme.of(context).textTheme.headlineMedium,
+                        ),
+                        Text(
+                          widget.whichAllergens,
+                          style: Theme.of(context).textTheme.headlineSmall,
+                        ),
+                        ElevatedButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          child: const Text('Go back!'),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
               ),
-              Text(
-                widget.ingredients,
-                style: const TextStyle(fontSize: 10),
-              ),
-              Text(
-                widget.canEat,
-                style: Theme.of(context).textTheme.headlineMedium,
-              ),
-              Text(
-                widget.whichAllergens,
-                style: Theme.of(context).textTheme.headlineSmall,
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                child: const Text('Go back!'),
-              ),
-            ],
+            ),
           ),
         ),
       ),
